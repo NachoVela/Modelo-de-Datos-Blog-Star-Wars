727f8430d9bb2828f53d68c1984a056dafbd546c
Nacho <ignaciovelamd@gmail.com>
2026-04-03 09:24:53 +0000

    terminando

--- src/models.py
+++ src/models.py
@@ -1,19 +1,96 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+Base = declarative_base()
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+class User(Base):
+    __tablename__ = "User"
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    
+    character_fav = relationship("Character_fav", back_populates="user")
+    planet_fav = relationship("Planet_fav", back_populates="user")
+    starship_fav = relationship("Starship_fav", back_populates="user")
+
+
+
+
+
+class Character(Base):
+    __tablename__ = "Character"
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+    height = Column(Float, nullable=False)
+    mass = Column(Float, nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+
+    character_fav = relationship("Character_fav", back_populates="character")
+
+class Character_fav(Base):
+    __tablename__ = "Character_fav"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    character_id = Column(Integer, ForeignKey("Character.id"))
+
+    user = relationship("User", back_populates="character_fav")
+    character = relationship("Character", back_populates="character_fav")
+
+
+
+
+
+class Planet(Base):
+    __tablename__ = "Planet"
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)   
+
+    planet_fav = relationship("Planet_fav", back_populates="planet")
+
+class Planet_fav(Base):
+    __tablename__ = "Planet_fav"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    planet_id = Column(Integer, ForeignKey("Planet.id"))
+
+    user = relationship("User", back_populates="planet_fav")
+    planet = relationship("Planet", back_populates="planet_fav")
+
+
+
+
+
+class Starship(Base):
+    __tablename__ = "Starship"
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cargo_capacity = Column(Integer, nullable=False)     
+
+    starship_fav = relationship("Starship_fav", back_populates="starship")
+    
+class Starship_fav(Base):
+    __tablename__ = "Starship_fav"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    starship_id = Column(Integer, ForeignKey("Starship.id"))
+
+    user = relationship("User", back_populates="starship_fav")
+    starship = relationship("Starship", back_populates="starship_fav")
+
+
+
+
+
+render_er(Base, "diagram.png")
